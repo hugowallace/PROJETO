904601ed7e6c30c2ead412519fc6b96d3e77cdc3
Hugo wallace <hugowallace@outlook.com.br>
2020-08-21 22:39:34 -0300

    adicionei alguns comentarios
    algumas funçoes nao estao funcionando

--- atividade/funcoes.py
+++ atividade/funcoes.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('banco1.db')
 cursor = conn.cursor()
 
-
+#nao esta funcionando
 def deletar_dados(tabela):
     cursor.execute(f"DELETE FROM {tabela}")
     conn.commit()
@@ -13,7 +13,7 @@ def deletar(id):
     cursor.execute(f"DELETE FROM motos_honda WHERE id = {id}")
     conn.commit()
 
-
+#nao esta funcionando
 def excluir_tabela(t):
     cursor.execute(f"DROP TABLE IF EXISTS {t}")
     conn.commit()
@@ -23,16 +23,16 @@ def inseir(n, p, c, cc):
     cursor.execute(f"INSERT INTO motos_honda(nome, preço, cilindrada, capacidade_combustivel) VALUES('{n}', '{p}', '{c}', '{cc}');")
     conn.commit()
 
-
+#nao esta funcionando
 def ver_tabelas(tabela):
     cursor.execute(f"SELECT * FROM {tabela}")
     for resultados in cursor.fectchall():
         print (resultados)
 
 
-
+#nao esta funcionando
 def ver_tabelas_id(id):
     cursor.execute(f"SELECT * FROM motos_honda WHERE id = {id}")
-    
-
+    for resultados in cursor.fectchall():
+        print (resultados)
 
